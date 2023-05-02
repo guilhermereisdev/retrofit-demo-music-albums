8cf0ca5587aba1be660162dd7bc995ea9a147a27
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-05-02 03:44:40 -0300

    Recupera os dados da API e os exibe na tela.

--- app/src/main/java/com/guilhermereisdev/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/guilhermereisdev/retrofitdemo/MainActivity.kt
@@ -2,10 +2,41 @@ package com.guilhermereisdev.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.guilhermereisdev.retrofitdemo.api.AlbumService
+import com.guilhermereisdev.retrofitdemo.api.RetrofitInstance
+import com.guilhermereisdev.retrofitdemo.model.Albums
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvAlbums: TextView = findViewById(R.id.tv_albums)
+
+        val retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+
+        responseLiveData.observe(this) {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    val result = "Album ID: ${albumsItem.id}\n" +
+                            "Album title: ${albumsItem.title}\n" +
+                            "User ID: ${albumsItem.userId}\n\n"
+                    tvAlbums.append(result)
+                }
+            }
+        }
     }
 }
